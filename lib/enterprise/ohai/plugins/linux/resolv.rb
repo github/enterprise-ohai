91a51286492da2e5c5362d0d006e389e693bdf0d
David Calavera <david.calavera@gmail.com>
2012-09-20 09:30:37 +0900

    fix resolv matching

--- lib/enterprise/ohai/plugins/linux/resolv.rb
+++ lib/enterprise/ohai/plugins/linux/resolv.rb
@@ -7,7 +7,7 @@ resolv_config = Mash.new
 
 if File.exist?(resolv_config_file)
   lines = File.readlines(resolv_config_file)
-  resolv_config[:name_servers] = lines.grep(/^nameserver #{ip}$/).map {|l| l[IP, 1] }
+  resolv_config[:name_servers] = lines.grep(/^nameserver #{ip}$/).map {|l| l[ip, 1] }
 end
 
 resolv resolv_config
